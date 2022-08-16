1f87d27c8c6acba45fafbfd177858a73de0ec6ce
Marlon <marlon_vs@yahoo.com>
2022-08-16 11:21:32 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,208 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+    {
+        id: 1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "./img/air.png",
+            },
+            {
+                code: "darkblue",
+                img: "./img/air2.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Air Jordan",
+        price: 149,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./img/jordan.png",
+            },
+            {
+                code: "green",
+                img: "./img/jordan2.png",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Blazer",
+        price: 109,
+        colors: [
+            {
+                code: "lightgray",
+                img: "./img/blazer.png",
+            },
+            {
+                code: "green",
+                img: "./img/blazer2.png",
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Crater",
+        price: 129,
+        colors: [
+            {
+                code: "black",
+                img: "./img/crater.png",
+            },
+            {
+                code: "lightgray",
+                img: "./img/crater2.png",
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [
+            {
+                code: "gray",
+                img: "./img/hippie.png",
+            },
+            {
+                code: "black",
+                img: "./img/hippie2.png",
+            },
+        ],
+    },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        //change the current slide
+        popUpContent.style.transform = `translateX(${-100 * index}vw)`;
+
+        //change the choosen product
+        choosenProduct = products[index];
+
+        //change texts of currentProduct
+        currentProductTitle.textContent = choosenProduct.title;
+        currentProductPrice.textContent = "$" + choosenProduct.price;
+        currentProductImg.src = choosenProduct.colors[0].img;
+
+        //assing new colors
+        currentProductColors.forEach((color, index) => {
+            color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
+    });
+});
+
+currentProductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+        currentProductImg.src = choosenProduct.colors[index].img;
+    });
+});
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+        currentProductSizes.forEach((size) => {
+            size.style.backgroundColor = "white";
+            size.style.color = "black";
+        });
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0px";
+    } else {
+        document.getElementById("nav").style.top = "-120px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event) {
+            document.querySelector(".popUpWrapper").style.display = "block"
+        },
+        2000
+    )
+});
+
+const popup = document.querySelector(".popUpWrapper");
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0];
+    const classNames = ['popUpClose', 'popUpWrapper'];
+    const shouldClosePopup = classNames.some(className => className === classNameOfClickedElement)
+
+    if (shouldClosePopup) {
+        popup.style.display = 'none'
+    }
+})
+
+const btnSlider = document.querySelector(".btnSlider");
+const img2 = document.querySelector(".img2");
+btnSlider.addEventListener("input",()=>{
+    let slideValue = btnSlider.value;
+    img2.style.clipPath = `polygon(0 0, ${slideValue}% 0, ${slideValue}% 100%,0 100%)`;
+});
+
+
+var gallery = document.querySelector('#gallery');
+var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
+var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+var resizeAll = function () {
+    var altura = getVal(gallery, 'grid-auto-rows');
+    var gap = getVal(gallery, 'grid-row-gap');
+    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+        var el = item;
+        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    });
+};
+gallery.querySelectorAll('img').forEach(function (item) {
+    item.classList.add('byebye');
+    if (item.complete) {
+        console.log(item.src);
+    }
+    else {
+        item.addEventListener('load', function () {
+            var altura = getVal(gallery, 'grid-auto-rows');
+            var gap = getVal(gallery, 'grid-row-gap');
+            var gitem = item.parentElement.parentElement;
+            gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+            item.classList.remove('byebye');
+        });
+    }
+});
+window.addEventListener('resize', resizeAll);
+gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+    item.addEventListener('click', function () {        
+        item.classList.toggle('full');        
+    });
+});
